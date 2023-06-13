47e80d40e8f9259046b8ff849476a2b86ed75e97
Via Listi Anggraeny <vialistiii30@gmail.com>
2023-06-13 16:24:08 +0700

    Fix : Color System

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
-  plugins: [],
-}
+	content: ['./src/**/*.{html,js}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Inter', ...defaultTheme.fontFamily.sans],
+			},
+			colors: {
+				primary: '#355b3e',
+				secondary: '#58745e',
+				tertiary: '#029664',
+				system: '#2f3d4c',
+				'system-light': '#f5dbc4',
+			},
+		},
+	},
+	plugins: [],
+};
